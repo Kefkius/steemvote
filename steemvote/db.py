b1397e0ff30332762ca46013e360041a38d000b3
Kefkius <kefkius@mail.com>
2016-08-23 21:48:05 -0400

    DB: Store and handle DB versions

--- steemvote/db.py
+++ steemvote/db.py
@@ -7,8 +7,15 @@ import plyvel
 
 from steemvote.models import Comment
 
+class DBVersionError(Exception):
+    """Exception raised when an incompatible database version is encountered."""
+    pass
+
 class DB(object):
     """Database for storing post data."""
+    # Current database version.
+    db_version = '0.1.0'
+
     @staticmethod
     def voted_key(identifier):
         """Get the key for a voted post with identifier."""
@@ -21,9 +28,32 @@ class DB(object):
         self.path = config.get('database_path', 'database/')
 
         self.db = plyvel.DB(self.path, create_if_missing=True)
+        self.check_version()
         self.comment_lock = threading.Lock()
 
         self.tracked_comments = {}
+
+    def check_version(self):
+        """Check the database version and update it if possible."""
+        version = self.get_version()
+        # Version is '0.0.0' if from before db version was stored.
+        if version < '0.1.0':
+            self.logger.warning('Incompatible or nonexistent database. Creating new database')
+            self.db.close()
+            plyvel.destroy_db(self.path)
+            self.db = plyvel.DB(self.path, create_if_missing=True)
+            self.set_version()
+        # Handle future db versions.
+        elif version > self.db_version:
+            raise DBVersionError('Stored database version (%s) is greater than current version (%s)' % (version, self.db_version))
+
+    def get_version(self):
+        """Get the stored database version."""
+        return str(self.db.get(b'db-version', b'0.0.0'), 'utf-8')
+
+    def set_version(self):
+        """Store the current database version."""
+        self.db.put(b'db-version', bytes(self.db_version, 'utf-8'))
 
     def load(self, steem):
         """Load state."""
