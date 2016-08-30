296ed07b163a740209fe289ca1617817e179d9a8
Kefkius <kefkius@mail.com>
2016-08-30 11:31:49 -0400

    Voter: Separate voting into a method
    Separate config loading into a method as well

--- steemvote/voter.py
+++ steemvote/voter.py
@@ -36,6 +36,9 @@ class Voter(object):
         # Last time that stats were updated via RPC.
         self.last_update = 0
 
+        self.config_lock = threading.Lock()
+        self.voting_lock = threading.Lock()
+
         # Load settings from config.
 
         config.require('voter_account_name')
@@ -44,31 +47,35 @@ class Voter(object):
         self.name = config.get('voter_account_name')
         self.wif = config.get('vote_key')
 
-        # Minimum age of posts to vote for.
-        self.min_post_age = config.get_seconds('min_post_age', DEFAULT_MIN_POST_AGE)
-        # Maximum age of posts to vote for.
-        self.max_post_age = config.get_seconds('max_post_age', DEFAULT_MAX_POST_AGE)
-
-        # Minimum available voting power.
-        self.min_voting_power = config.get_decimal('min_voting_power', DEFAULT_MINIMUM_VOTING_POWER)
-        # Maximum available voting power.
-        # Steemvote will attempt to use more power than normal if current
-        # voting power is greater than this.
-        self.max_voting_power = config.get_decimal('max_voting_power', DEFAULT_MAXIMUM_VOTING_POWER)
-        # The maximum voting power must not be less than the minimum voting power.
-        if self.max_voting_power < self.min_voting_power:
-            raise ConfigError('"max_voting_power" must not be less than "min_voting_power"')
-
-        # Categories to ignore posts in.
-        self.blacklisted_categories = config.get('blacklist_categories', [])
-
-        self.rpc_node = config.get('rpc_node')
-        self.rpc_user = config.get('rpc_user')
-        self.rpc_pass = config.get('rpc_pass')
-
+        self.load_settings()
 
         self.db = DB(config)
-        self.voting_lock = threading.Lock()
+
+    def load_settings(self):
+        """Load settings from config."""
+        with self.config_lock:
+            config = self.config
+            # Minimum age of posts to vote for.
+            self.min_post_age = config.get_seconds('min_post_age', DEFAULT_MIN_POST_AGE)
+            # Maximum age of posts to vote for.
+            self.max_post_age = config.get_seconds('max_post_age', DEFAULT_MAX_POST_AGE)
+
+            # Minimum available voting power.
+            self.min_voting_power = config.get_decimal('min_voting_power', DEFAULT_MINIMUM_VOTING_POWER)
+            # Maximum available voting power.
+            # Steemvote will attempt to use more power than normal if current
+            # voting power is greater than this.
+            self.max_voting_power = config.get_decimal('max_voting_power', DEFAULT_MAXIMUM_VOTING_POWER)
+            # The maximum voting power must not be less than the minimum voting power.
+            if self.max_voting_power < self.min_voting_power:
+                raise ConfigError('"max_voting_power" must not be less than "min_voting_power"')
+
+            # Categories to ignore posts in.
+            self.blacklisted_categories = config.get('blacklist_categories', [])
+
+            self.rpc_node = config.get('rpc_node')
+            self.rpc_user = config.get('rpc_user')
+            self.rpc_pass = config.get('rpc_pass')
 
     def connect_to_steem(self):
         """Connect to a Steem node."""
@@ -91,7 +98,12 @@ class Voter(object):
         if now - self.last_update < self.update_interval:
             return
 
-        d = self.steem.rpc.get_accounts([self.name])[0]
+        response = self.steem.rpc.get_accounts([self.name])
+        try:
+            d = response[0]
+        except Exception as e:
+            self.logger.error('Invalid get_accounts() response: %s' % response)
+            raise e
         # Calculate our current voting power.
         self.current_voting_power = d['voting_power'] / 10000.0
 
@@ -118,24 +130,41 @@ class Voter(object):
             bool, and reason contains the reason not to vote
             if should_vote is False.
         """
-        # Do not vote if the post has curation disabled.
-        if not comment.allow_curation_rewards or not comment.allow_votes:
-            return False
-        author = self.config.get_author(comment.author, self.use_backup_authors())
-        if not author:
-            return (False, 'author is unknown')
-        if comment.is_reply() and not author.vote_replies:
-            return (False, 'comment is a reply')
-        # Do not vote if the post is in a blacklisted category.
-        if comment.category in self.blacklisted_categories:
-            return (False, 'comment is in a blacklisted category')
-        # Do not vote if the post is too old.
-        if time.time() - comment.timestamp > self.max_post_age:
-            return (False, 'comment is too old')
-        # Do not vote if we're using too much voting power.
-        if self.current_voting_power < self.min_voting_power:
-            return (False, 'voter does not have enough voting power (current: %s)' % self.get_voting_power())
-        return (True, '')
+        with self.config_lock:
+            # Do not vote if the post has curation disabled.
+            if not comment.allow_curation_rewards or not comment.allow_votes:
+                return False
+            author = self.config.get_author(comment.author, self.use_backup_authors())
+            if not author:
+                return (False, 'author is unknown')
+            if comment.is_reply() and not author.vote_replies:
+                return (False, 'comment is a reply')
+            # Do not vote if the post is in a blacklisted category.
+            if comment.category in self.blacklisted_categories:
+                return (False, 'comment is in a blacklisted category')
+            # Do not vote if the post is too old.
+            if time.time() - comment.timestamp > self.max_post_age:
+                return (False, 'comment is too old')
+            # Do not vote if we're using too much voting power.
+            if self.current_voting_power < self.min_voting_power:
+                return (False, 'voter does not have enough voting power (current: %s)' % self.get_voting_power())
+            return (True, '')
+
+    def _vote(self, identifier, weight):
+        """Create and broadcast a vote for identifier."""
+        tx = self.steem.vote(identifier, weight, voter=self.name)
+        try:
+            self.steem.rpc.broadcast_transaction(tx, api='network_broadcast')
+            self.logger.info('Voted on %s' % identifier)
+        except grapheneapi.graphenewsrpc.RPCError as e:
+            already_voted_messages = [
+                'Changing your vote requires',
+                'Cannot vote again',
+            ]
+            if e.args and any(i in e.args[0] for i in already_voted_messages):
+                self.logger.info('Skipping already-voted post %s' % identifier)
+            else:
+                raise e
 
     def vote_for_comments(self):
         """Vote on the comments that are ready."""
@@ -151,18 +180,6 @@ class Voter(object):
                     self.logger.debug('Skipping %s because %s' % (comment.identifier, reason))
                     continue
                 author = self.config.get_author(comment.author, self.use_backup_authors())
-                tx = self.steem.vote(comment.identifier, author.weight, voter=self.name)
-                try:
-                    self.steem.rpc.broadcast_transaction(tx, api='network_broadcast')
-                    self.logger.info('Voted on %s' % comment.identifier)
-                except grapheneapi.graphenewsrpc.RPCError as e:
-                    already_voted_messages = [
-                        'Changing your vote requires',
-                        'Cannot vote again',
-                    ]
-                    if e.args and any(i in e.args[0] for i in already_voted_messages):
-                        self.logger.info('Skipping already-voted post %s' % comment.identifier)
-                    else:
-                        raise e
+                self._vote(comment.identifier, author.weight)
 
             self.db.update_voted_comments(comments)
